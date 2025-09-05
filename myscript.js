38db9fccd872a66bc1065492127925b974179771
Ann Jordan <annie.tru.jordan@gmail.com>
2025-09-05 12:45:56 -0700

    Feat: add quiz format

--- myscript.js
+++ myscript.js
@@ -1,7 +1,41 @@
-$(document).ready(function() {
 
-	$("#quetion1").click(function() {
-		$(".answer1").toggle();
-	});
+$(document).ready(function () {
+	$(".question-text").on("click", function () {
+	$(this).siblings(".answer").slideToggle();
+  });
 
+  $(".answer").on("click", function () {
+    let $selected = $(this);
+    let $question = $selected.closest(".question"); // traverse to the ul
+    let $answers = $question.children(".answer");   // get sibling answers
+
+    if ($selected.hasClass("selected")) return;
+
+    $selected.addClass("selected");
+
+    $answers.css("pointer-events", "none");
+
+    if ($selected.hasClass("correct")) {
+      $selected.addClass("correct-answer");
+      
+      $selected.find(".explanation").slideDown();
+    } else {
+      $selected.addClass("incorrect-answer");
+    }
+  });
 });
+
+
+/*
+$(document).ready(function() {
+ 
+	$("ul").click(function(){
+		$(this).children().css("visibility", "visible");
+	});
+	
+	$("li").click(function(){
+		$(this).children().css("visibility", "visible");
+	});
+	
+});
+*/
