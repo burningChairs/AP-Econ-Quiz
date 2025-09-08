9c3c4333611560fcc27ce5d71e442f5fbf82d0fb
Ann Jordan <annie.tru.jordan@gmail.com>
2025-09-08 14:06:18 -0700

    Fix: fix comment code

--- myscript.js
+++ myscript.js
@@ -1,41 +1,40 @@
 
 $(document).ready(function () {
 	$(".question-text").on("click", function () {
-	$(this).siblings(".answer").slideToggle();
+		$(this).siblings(".answer").slideToggle();
   });
 
-  $(".answer").on("click", function () {
-    let $selected = $(this);
-    let $question = $selected.closest(".question"); // traverse to the ul
-    let $answers = $question.children(".answer");   // get sibling answers
+	$(".answer").on("click", function () {
+		let $selected = $(this);
+		let $question = $selected.closest(".question"); 
+		let $answers = $question.children(".answer"); 
 
-    if ($selected.hasClass("selected")) return;
+		if ($selected.hasClass("selected")) return;
 
-    $selected.addClass("selected");
+		$selected.addClass("selected");
 
-    $answers.css("pointer-events", "none");
+		$answers.css("pointer-events", "none");
 
-    if ($selected.hasClass("correct")) {
-      $selected.addClass("correct-answer");
+		if ($selected.hasClass("correct")) {
+			$selected.addClass("correct-answer");
       
-      $selected.find(".explanation").slideDown();
-    } else {
-      $selected.addClass("incorrect-answer");
+			$selected.find(".explanation").slideDown();
+		} else {
+			$selected.addClass("incorrect-answer");
     }
   });
+  
+   $(document).ready(function() {
+      $('#add-comment').click(function() {
+        const commentText = $('#comment-input').val().trim();
+        if (commentText) {
+          $('#comment-list').append(`<div class="comment">${commentText}</div>`);
+          $('#comment-input').val('');
+        } else {
+          alert('Please write a comment before submitting.');
+        }
+      });
+    });
+  
+  
 });
-
-
-/*
-$(document).ready(function() {
- 
-	$("ul").click(function(){
-		$(this).children().css("visibility", "visible");
-	});
-	
-	$("li").click(function(){
-		$(this).children().css("visibility", "visible");
-	});
-	
-});
-*/
